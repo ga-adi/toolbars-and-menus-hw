1a52cc21af3061c0b7e9a98bc7ac41f8725d7d92
roberrera <roberterrera@gmail.com>
2016-02-22 09:13:13 -0500

    Added TED toolbar

--- app/src/main/java/com/example/roberrera/toolbars_and_menus_hw/MainActivity.java
+++ app/src/main/java/com/example/roberrera/toolbars_and_menus_hw/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.roberrera.toolbars_and_menus_hw;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -17,13 +19,14 @@ public class MainActivity extends AppCompatActivity {
     ImageView mAlbumArt;
     ImageButton mPlayButton;
     TextView mEpisodeName, mArtistName;
+    Button mToTedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("Toolbars & Menus");
+        setTitle("Pocketcasts");
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -32,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         mEpisodeName = (TextView)findViewById(R.id.textview_songtitle);
         mArtistName = (TextView)findViewById(R.id.textview_artist);
         mPlayButton = (ImageButton) findViewById(R.id.image_play);
+        mToTedButton = (Button) findViewById(R.id.button_toTed);
 
         mEpisodeName.setText("Episode 3002: PS7 Brain Implants");
         mArtistName.setText("P.S. I Love You (XOXO)");
@@ -40,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Playing " + mEpisodeName.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mToTedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TedActivity.class);
+                startActivity(intent);
             }
         });
 
