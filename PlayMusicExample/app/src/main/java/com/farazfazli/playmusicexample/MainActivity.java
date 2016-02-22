3250239062acb6d3912d552c13092b6e1286387f
Faraz Fazli <farazfazli@gmail.com>
2016-02-22 09:25:47 -0500

    Add music player

---
+++ PlayMusicExample/app/src/main/java/com/farazfazli/playmusicexample/MainActivity.java
@@ -0,0 +1,37 @@
+package com.farazfazli.playmusicexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_share:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
